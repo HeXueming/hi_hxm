1beea176457c4ef53e629b2f718c0936f0fd3d19
HeXueming <943240641@qq.com>
2020-03-30 15:00:45 +0800

    test_2

--- src/main/java/com/example/demo/Constroller/Hi_hxm.java
+++ src/main/java/com/example/demo/Constroller/Hi_hxm.java
@@ -1,4 +1,13 @@
 package com.example.demo.Constroller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Hi_hxm {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello.html";
+    }
 }
